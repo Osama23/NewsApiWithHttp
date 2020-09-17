2186414f2d4078f74f828e53a59006584c90e426
Osama Abdelaziz <oabdelaziz@ntgclarity.com>
2020-09-18 00:54:37 +0200

    First Commit

---
+++ lib/services/api_manager.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:NewsApp/constants/strings.dart';
+import 'package:NewsApp/model/newsInfo.dart';
+import 'package:http/http.dart' as http;
+
+
+class API_Manager {
+  Future<NewsModel> getNews() async {
+    var client = http.Client();
+    var newsModel;
+
+    try {
+      var response = await client.get(Strings.news_url);
+      if (response.statusCode == 200) {
+        var jsonString = response.body;
+        var jsonMap = json.decode(jsonString);
+
+        newsModel = NewsModel.fromJson(jsonMap);
+      }
+    } catch (Exception) {
+      return newsModel;
+    }
+
+    return newsModel;
+  }
+}
